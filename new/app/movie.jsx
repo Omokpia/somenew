98fd925e5f4379ee3f250a5af2a01c5a19d3cec7
Godspower Omokpia <omokpia59@gmail.com>
2025-09-19 20:19:57 +0100

    Update movie.jsx

--- new/app/movie.jsx
+++ new/app/movie.jsx
@@ -6,11 +6,77 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 
 
-const MovieScreen = () => {
+ const MovieScreen = () => {
+
+
+
+//     function getMovies() {
+
+        // fetch(url, {
+        // method: 'DELETE',
+        // headers: {
+        //     'x-rapidapi-key': 'c0a325639emsh7652e48950072a3p108dc3jsnf7226afa7c38',
+		//     'x-rapidapi-host': 'imdb236.p.rapidapi.com'
+        // }
+    
+        // })
+
+
+//         try{
+
+//         }
+//         catch(error){
+
+//         }
+//         finally{
+
+//         }
+
+
+
+
+
+
+
+
+
+//     }
+
+
+
+
+//     const url = 'https://imdb236.p.rapidapi.com/api/imdb/search?type=movie&genre=Drama&rows=25&sortOrder=ASC&sortField=id';
+
+//     const options = {
+
+// 	method: 'GET',
+    
+// 	headers: {
+// 		'x-rapidapi-key': 'c0a325639emsh7652e48950072a3p108dc3jsnf7226afa7c38',
+// 		'x-rapidapi-host': 'imdb236.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+
+// } catch (error) {
+
+// 	console.error(error);
+// }
+
+
+
+
+
+
     return(
         <SafeAreaView>
 
-            <Text style={{color:'black', fontSize:21, 
+            <Text style={{color:'black', fontSize:30, 
                 alignSelf:"center"}}>Movie screen</Text>
 
         </SafeAreaView>
